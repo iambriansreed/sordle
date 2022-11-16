e1205960a19c581b07e6d32dc9cc092b593ff4f3
Brian S. Reed <iambriansreed@gmail.com>
2022-11-16 17:51:09 -0500

    lots of fixes

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import './main.scss';
 
 const VERSION = '0.1.0';
 
-const MAX_ATTEMPTS = 6;
+const MAX_ATTEMPTS = 2;
 
 const WORD_LENGTH = 5;
+
+const CHAR_CARD = `<div class="char"><div class="body"><div class="front"></div><div class="back"></div></div></div>`;
 
 type Word = {
     word: string;
@@ -23,19 +25,51 @@ type Word = {
     }[];
 };
 
-function cookieGet(str: string) {
-    const cookieValue = document.cookie.match(new RegExp(`(^| )__sordle__${str}=([^;]+)`))?.[2] || '';
-    try {
-        return JSON.parse(decodeURIComponent(cookieValue));
-    } catch {
-        return null;
-    }
+function useNotifications() {
+    const $notify = $('.notify');
+
+    let showHelperTimeout: ReturnType<typeof setTimeout>;
+
+    const hideNotify = () => {
+        $notify.style.opacity = '0';
+        showHelperTimeout = setTimeout(() => {
+            $notify.innerHTML = '';
+        }, 200);
+    };
+
+    const notify = (str: string, autoClose = true) => {
+        if (showHelperTimeout) clearTimeout(showHelperTimeout);
+        $notify.innerHTML = str;
+
+        setTimeout(() => {
+            $notify.style.opacity = '1';
+        }, 1);
+
+        if (autoClose)
+            showHelperTimeout = setTimeout(() => {
+                hideNotify();
+            }, 1500);
+    };
+
+    return {
+        notify,
+        hideNotify,
+    };
 }
 
-function cookieSet<T>(str: string, value: T) {
-    const cookieValue = encodeURIComponent(JSON.stringify(value));
-    document.cookie = `__sordle__${str}=${cookieValue}; SameSite=None; Secure`;
-}
+// function cookieGet(str: string) {
+//     const cookieValue = document.cookie.match(new RegExp(`(^| )__sordle__${str}=([^;]+)`))?.[2] || '';
+//     try {
+//         return JSON.parse(decodeURIComponent(cookieValue));
+//     } catch {
+//         return null;
+//     }
+// }
+
+// function cookieSet<T>(str: string, value: T) {
+//     const cookieValue = encodeURIComponent(JSON.stringify(value));
+//     document.cookie = `__sordle__${str}=${cookieValue}; SameSite=None; Secure`;
+// }
 
 const timeoutPromise = async <T>(ms: number, callback?: () => T): Promise<T> =>
     await new Promise((resolve) =>
@@ -52,16 +86,9 @@ const $ = (selector: string, container: ParentNode = document): HTMLElement =>
 
 (async () => {
     const $app = $('.app');
-    const $helper = $('.helper');
-
-    const $buttons: Record<string, HTMLElement> = {};
-    $$('[data-key]').forEach((element) => {
-        $buttons[element.dataset.key!] = element;
-    });
-
-    const $attempts = $('.attempts');
-
     const $overlay = $('.overlay');
+    const $main = $('main');
+    const mainContent = $('main').innerHTML;
 
     const modalContent: Record<'welcome' | 'success' | 'fail', string> = {
         welcome: $overlay.innerHTML,
@@ -69,36 +96,63 @@ const $ = (selector: string, container: ParentNode = document): HTMLElement =>
         fail: $('[data-modal="fail"]').innerHTML,
     } as const;
 
-    const checkWord = async (value: string): Promise<Word | null> =>
-        window
-            .fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + value)
-            .then((r) => r.json())
-            .then((response) => {
-                if (Array.isArray(response) && response.length) return response[0] as Word;
-                return null;
-            })
-            .catch(() => null);
+    const getJson = async <T>(url: string) => {
+        try {
+            return (
+                window
+                    .fetch(url)
+                    .then((r) => r.json())
+                    .catch(() => null) || null
+            );
+        } catch {
+            // I don't care about 404s
+            return null;
+        }
+    };
+
+    const { hideNotify, notify } = useNotifications();
 
     let wordMeanings: Word;
     let word: string[] = [];
-    const setWord = (next: typeof word) => {
-        word = next;
+    let $attempts: HTMLElement;
+    const $keyboardKeys: Record<string, HTMLElement> = {};
 
-        const char = `<div class="char"><div class="body"><div class="front"></div><div class="back"></div></div></div>`;
-        $attempts.innerHTML = `<div class="attempt">${char.repeat(WORD_LENGTH)}</div>`.repeat(MAX_ATTEMPTS);
+    const resetMain = (newWord: string[], newWordMeanings: Word) => {
+        hideNotify();
 
-        Object.values($buttons).forEach((element) => element.classList.remove('yellow', 'green', 'none'));
+        word = newWord;
+        wordMeanings = newWordMeanings;
+
+        $main.innerHTML = mainContent;
+
+        $attempts = $('.attempts', $main);
+
+        $attempts.innerHTML = `<div class="attempt">${CHAR_CARD.repeat(WORD_LENGTH)}</div>`.repeat(MAX_ATTEMPTS);
+
+        setAttempt({ index: 0, chars: [] });
+
+        setTimeout(() => {
+            $$('[data-key]', $main).forEach((element) => {
+                $keyboardKeys[element.dataset.key] = element;
+            });
+        }, 10);
     };
 
-    setWord(['h', 'e', 'l', 'l', 'o']); // there 👋
+    const checkWord = async (value: string): Promise<Word | null> =>
+        getJson(`https://api.dictionaryapi.dev/api/v2/entries/en/${value}`).then((response) => {
+            if (Array.isArray(response) && response.length) return response[0] as Word;
+            return null;
+        });
 
     let attempt = {
         chars: [] as string[],
         index: 0,
     };
+
     const setAttempt = (next: typeof attempt) => {
         attempt = next;
         const attemptRow = $attempts.childNodes[attempt.index];
+
         for (let charIndex = 0; charIndex < WORD_LENGTH; charIndex++) {
             const char = attempt.chars[charIndex];
             const element = attemptRow.childNodes[charIndex] as HTMLElement;
@@ -110,7 +164,7 @@ const $ = (selector: string, container: ParentNode = document): HTMLElement =>
     const keys: Record<string, 'yellow' | 'green' | 'none'> = {};
 
     let flipping = false;
-    const animateFlip = async (row: HTMLElement, ms = 300) =>
+    const attemptFlip = async (row: HTMLElement, ms = 300) =>
         await Promise.all([
             ...word.map(async (_, i) =>
                 timeoutPromise(i * ms, () => {
@@ -138,124 +192,40 @@ const $ = (selector: string, container: ParentNode = document): HTMLElement =>
 
     let getNewWordCount = 0;
     const getNewWord = async () => {
-        const tempWord = await window
-            .fetch('https://random-word-api.herokuapp.com/word?length=' + WORD_LENGTH)
-            .then((r) => r.json())
-            .then((words) => {
-                return words[0];
-            });
+        notify('Loading new word', false);
 
-        const tempWordMeanings = await checkWord(tempWord);
+        const tempWord = await getJson<string>(`https://random-word-api.herokuapp.com/word?length=${WORD_LENGTH}`).then(
+            (words) => words?.[0]
+        );
+
+        const tempWordMeanings = tempWord ? await checkWord(tempWord) : null;
+
+        // success
 
         if (tempWordMeanings) {
-            wordMeanings = tempWordMeanings;
-            setWord(tempWord.split(''));
+            resetMain(tempWord.split(''), tempWordMeanings);
             return;
         }
 
+        // failure - random word not in the dictionary
+
         if (getNewWordCount > 5) {
-            showHelper('Still loading new word', false);
-            return;
+            notify('Still loading new word', false);
         }
 
         if (getNewWordCount > 10) {
-            showHelper('Error loading new word', false);
-            return;
+            notify('Error loading new word', false);
         }
 
         getNewWordCount++;
         getNewWord();
     };
 
-    const loadNewWord = async () => {
-        $overlay.innerHTML = '';
-
-        showHelper('Loading new word', false);
-
-        await getNewWord();
-
-        showHelper('Loading new word');
-
-        setAttempt({ index: 0, chars: [] });
-    };
-
-    let showHelperTimeout: ReturnType<typeof setTimeout>;
-    const showHelper = (str: string, autoClose = true) => {
-        if (showHelperTimeout) clearTimeout(showHelperTimeout);
-        $helper.innerHTML = str;
+    const loadDefinitions = () => {
+        if (!wordMeanings?.meanings) return;
 
         setTimeout(() => {
-            $helper.style.opacity = '1';
-        }, 1);
-
-        if (autoClose)
-            showHelperTimeout = setTimeout(() => {
-                $helper.style.opacity = '0';
-                showHelperTimeout = setTimeout(() => {
-                    $helper.innerHTML = '';
-                }, 200);
-            }, 1500);
-    };
-
-    const handleEnter = async () => {
-        if (attempt.chars.length < WORD_LENGTH) {
-            animateError();
-            showHelper('Not enough letters');
-            return;
-        }
-
-        const invalidWord = !(await checkWord(attempt.chars.join('')));
-
-        if (invalidWord) {
-            animateError('row');
-            showHelper('Not in word list');
-            return;
-        }
-
-        const currentAttemptRow = $attempts.childNodes[attempt.index] as HTMLElement;
-
-        const currentAttemptElements = currentAttemptRow.childNodes;
-
-        let correct = 0;
-
-        attempt.chars.forEach((char, charIndex) => {
-            const element = currentAttemptElements[charIndex] as HTMLElement;
-
-            if (word[charIndex] === char) {
-                keys[char] = 'green';
-                element.classList.add('green');
-                correct++;
-            } else if (word.includes(char)) {
-                keys[char] = keys[char] || 'yellow';
-                element.classList.add('yellow');
-            } else {
-                keys[char] = 'none';
-                element.classList.add('none');
-            }
-        });
-
-        await animateFlip(currentAttemptRow);
-
-        Object.entries(keys).forEach(([char, color]) => {
-            $buttons[char].classList.add(color);
-        });
-
-        if (correct === WORD_LENGTH) {
-            $overlay.innerHTML = modalContent.success;
-            setTimeout(() => {
-                $('[data-attempt-text]', $overlay).innerText = ` ${attempt.index} ${
-                    attempt.index ? 'attempts' : 'attempt'
-                }`;
-            }, 1);
-        }
-
-        if (attempt.index + 1 === MAX_ATTEMPTS) {
-            $overlay.innerHTML = modalContent.fail;
-        }
-
-        if ($overlay.innerHTML && wordMeanings?.meanings) {
-            setTimeout(() => {
-                $('[data-definition]', $overlay).innerHTML = `<h3>${word.join('')}</h3>
+            $('[data-definition]', $overlay).innerHTML = `<h3>${word.join('')}</h3>
 <ul class="meanings">
     ${wordMeanings?.meanings
         .map(
@@ -277,7 +247,67 @@ const $ = (selector: string, container: ParentNode = document): HTMLElement =>
         )
         .join('')}
 </ul>`;
+        }, 1);
+    };
+
+    const handleEnter = async () => {
+        if (attempt.chars.length < WORD_LENGTH) {
+            animateError();
+            notify('Not enough letters');
+            return;
+        }
+
+        const invalidWord = !(await checkWord(attempt.chars.join('')));
+
+        if (invalidWord) {
+            animateError('row');
+            notify('Not in word list');
+            return;
+        }
+
+        const currentAttemptRow = $attempts.childNodes[attempt.index] as HTMLElement;
+
+        const currentAttemptElements = currentAttemptRow.childNodes;
+
+        let correct = 0;
+
+        attempt.chars.forEach((char, charIndex) => {
+            const element = currentAttemptElements[charIndex] as HTMLElement;
+
+            const backElement = element.firstChild.childNodes[1] as HTMLElement;
+            if (word[charIndex] === char) {
+                keys[char] = 'green';
+                backElement.classList.add('green');
+                correct++;
+            } else if (word.includes(char)) {
+                keys[char] = keys[char] || 'yellow';
+                backElement.classList.add('yellow');
+            } else {
+                keys[char] = 'none';
+                backElement.classList.add('none');
+            }
+        });
+
+        await attemptFlip(currentAttemptRow);
+
+        Object.entries(keys).forEach(([char, color]) => {
+            $keyboardKeys[char].classList.add(color);
+        });
+
+        if (correct === WORD_LENGTH) {
+            $overlay.innerHTML = modalContent.success;
+
+            setTimeout(() => {
+                $('[data-attempt-text]', $overlay).innerText = ` ${attempt.index} ${
+                    attempt.index ? 'attempts' : 'attempt'
+                }`;
             }, 1);
+            loadDefinitions();
+            getNewWord();
+        } else if (attempt.index + 1 === MAX_ATTEMPTS) {
+            $overlay.innerHTML = modalContent.fail;
+            loadDefinitions();
+            getNewWord();
         }
 
         setAttempt({ chars: [], index: attempt.index + 1 });
@@ -293,8 +323,8 @@ const $ = (selector: string, container: ParentNode = document): HTMLElement =>
             return;
         }
 
-        if (targetElement.dataset.action === 'loadWord') {
-            loadNewWord();
+        if (targetElement.dataset.action === 'closeModal') {
+            $overlay.innerHTML = '';
             return;
         }
 
@@ -322,13 +352,13 @@ const $ = (selector: string, container: ParentNode = document): HTMLElement =>
         setAttempt(attempt);
     };
 
-    const existingWord = cookieGet('word');
-    if (existingWord) {
-        $overlay.innerHTML = '';
-        setWord(existingWord.split(''));
-    }
-
     $app.addEventListener('click', handleClick);
 
     $('[data-version]').innerText = VERSION;
+
+    $overlay.addEventListener('click', () => {
+        $overlay.innerHTML = '';
+    });
+
+    getNewWord();
 })();
