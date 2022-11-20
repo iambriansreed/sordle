fda59e2c58ca000308c990e9a3f654e8e427d1d3
Brian S. Reed <iambriansreed@gmail.com>
2022-11-19 23:40:53 -0500

    add service worker

--- src/main.ts
+++ src/main.ts
@@ -412,3 +412,12 @@ const $ = (selector: string, container: ParentNode = document): HTMLElement =>
 
     getNewWord();
 })();
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/serviceWorker.js')
+            .then((res) => console.log('service worker registered'))
+            .catch((err) => console.log('service worker not registered', err));
+    });
+}
