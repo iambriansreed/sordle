b61f6804288497e48a6ad3ab7f087ab6591c2e09
Brian Reed <iambriansreed@gmail.com>
2024-06-21 20:28:26 -0400

    remove service worker

--- src/main.ts
+++ src/main.ts
@@ -293,24 +293,3 @@ function app() {
 }
 
 app();
-
-const registerServiceWorker = async () => {
-    if ('serviceWorker' in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register('/sw.js', {
-                scope: '/',
-            });
-            if (registration.installing) {
-                console.info('Service worker installing');
-            } else if (registration.waiting) {
-                console.info('Service worker installed');
-            } else if (registration.active) {
-                console.info('Service worker active');
-            }
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
-        }
-    }
-};
-
-registerServiceWorker();
